3d557a1659b67861c849ec00508f1ec80de52395
Ndeye456 <tndeyeamie456@gmail.com>
2023-07-26 17:51:32 +0000

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
+import { Store } from "../utils/Store";
 
 export default function Layout({ title, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
   return (
     <>
       <Head>
@@ -18,14 +21,23 @@ export default function Layout({ title, children }) {
               LadyFashion
             </Link>
             <div>
-              <Link href="/cart" className="p-2">Panier</Link>
-              <Link href="/login" className="p-2">Se connecter</Link>
+              <Link href="/cart" className="p-2">
+                Panier
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a,c) => a+c.quantity, 0)}
+                  </span>  
+                )}
+              </Link>
+              <Link href="/login" className="p-2">
+                Se connecter
+              </Link>
             </div>
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
         <footer className="flex h-10 justify-center items-center shadow-inner">
-            <p>Copyright @ 2023 LadyFashion</p>
+          <p>Copyright @ 2023 LadyFashion</p>
         </footer>
       </div>
     </>
